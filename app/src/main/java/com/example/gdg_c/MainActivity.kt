715d8dbfc6390f45576cb82451b33e3bad615dc4
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-18 22:07:35 +0900

    [feat]: 날라간 기록 복구

--- app/src/main/java/com/example/gdg_c/MainActivity.kt
+++ app/src/main/java/com/example/gdg_c/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.gdg_c
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.gdg_c.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
+        setBottomNaviVisible(navController)
+    }
+
+    private fun setBottomNaviVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMain.visibility = when (destination.id) {
+                R.id.fragment_task, R.id.fragment_report, R.id.fragment_my -> View.VISIBLE
+                else -> View.GONE
+            }
         }
     }
 }

a95d6162ca82fd5f1fda1fc45677b36cd58c48de
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-19 01:27:34 +0900

    [feat](data): My Info Api 리스폰스 , repository 구현 (#9)

--- app/src/main/java/com/example/gdg_c/data/repository/MyRepository.kt
+++ app/src/main/java/com/example/gdg_c/data/repository/MyRepository.kt
@@ -1,4 +1,14 @@
 package com.example.gdg_c.data.repository
 
+import com.example.gdg_c.data.model.repsonse.BaseResponse
+import com.example.gdg_c.data.model.repsonse.my.MyInfoResponse
+import com.example.gdg_c.data.network.RetrofitInstance
+
 class MyRepository {
+
+    suspend fun getMyInfo(userIdentity: String): BaseResponse<MyInfoResponse> {
+        val response = RetrofitInstance.myService.getMyInfo(userIdentity)
+
+        return response
+    }
 }
